2a4161b5507c6a874bedae4828bc15c01516e503
Kamyar Dabiri <kdabiria@uci.edu>
2021-06-06 12:45:33 -0700

    username and password validation

--- app/src/main/java/com/example/bank/RegisterActivity.java
+++ app/src/main/java/com/example/bank/RegisterActivity.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Pattern;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private static final String TAG = "RegisterActivity";
@@ -24,7 +26,7 @@ public class RegisterActivity extends AppCompatActivity {
     DatabaseHelper mDatabaseHelper;
     private boolean check;
     private Float newEntry5;
-
+    boolean balanceCheck = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,26 +49,35 @@ public class RegisterActivity extends AppCompatActivity {
                 String newEntry = firstName.getText().toString();
                 String newEntry2 = lastName.getText().toString();
                 String newEntry3 = username.getText().toString();
+                boolean checkUsername = isValidUsername(newEntry3);
+                Log.d(TAG, "username validation: " + checkUsername);
                 String newEntry4 = password.getText().toString();
+                boolean checkPass = isValidPassword(newEntry4);
                 String newEntry6 = email.getText().toString();
                 boolean checkEmail = isValidEmail(newEntry6);
-                boolean balanceCheck = checkBalance(balance.getText().toString());
+                if(balance.getText().toString().length() != 0)
+                    balanceCheck = checkBalance(balance.getText().toString());
                 if(balanceCheck)
                     newEntry5 = Float.parseFloat(balance.getText().toString());
-                if(newEntry3.length() != 0 && newEntry4.length() != 0 && newEntry.length() != 0 && newEntry2.length() != 0) {
-                    if(checkEmail) {
-                        if (balanceCheck) {
-                            check = AddData(newEntry, newEntry2, newEntry3, newEntry4, newEntry5, newEntry6);
-                            Log.d(TAG, check + "!!!!!");
-                            if (check) {
-                                toastMeesage("Successfully Created Account!");
-                                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                            }
+                if(newEntry3.length() != 0 && newEntry4.length() != 0 && newEntry.length() != 0 && newEntry2.length() != 0 && newEntry5 != null) {
+                    if(checkUsername) {
+                        if (checkPass) {
+                            if (checkEmail) {
+                                if (balanceCheck) {
+                                    check = AddData(newEntry, newEntry2, newEntry3, newEntry4, newEntry5, newEntry6);
+                                    if (check) {
+                                        toastMeesage("Successfully Created Account!");
+                                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                                    }
+                                } else
+                                    toastMeesage("Balance Invalid Input");
+                            } else
+                                toastMeesage("Invalid Email");
                         } else
-                            toastMeesage("Balance Invalid Input");
+                            toastMeesage("Invalid Password");
                     }
                     else
-                        toastMeesage("Invalid Email");
+                        toastMeesage("Invalid Username");
                 }
                 else
                     toastMeesage("You must fill the required fields!");
@@ -111,6 +122,14 @@ public class RegisterActivity extends AppCompatActivity {
 
     private boolean isValidEmail(String email) {
         return !email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private boolean isValidPassword(String pass) {
+        return Pattern.matches("[_\\-\\.0-9a-z]*",pass) && pass.length() >= 1 && pass.length() <= 127;
+    }
+
+    private boolean isValidUsername(String username) {
+        return Pattern.matches("[_\\-\\.0-9a-z]*",username) && username.length() >= 1 && username.length() <= 127 ;
     }
 
     private void toastMeesage(String message) {
